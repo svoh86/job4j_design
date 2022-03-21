6ba5fcc4afdf9cec76754d0bf7ebac43297803d0
svoh86 <sms-86@mail.ru>
2022-03-21 14:57:49 +0300

    Создать итератор четные числа [#150]

---
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -0,0 +1,44 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 5.1.2. Создать итератор четные числа
+ * Класс описывает итератор, который возвращающий только четные цифры
+ *
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    /**
+     * Метод проверяет, что в массиве есть четные числа
+     *
+     * @return true или  false
+     */
+    @Override
+    public boolean hasNext() {
+        while (index < data.length) {
+            if (data[index] % 2 == 0) {
+                return true;
+            }
+            index++;
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}

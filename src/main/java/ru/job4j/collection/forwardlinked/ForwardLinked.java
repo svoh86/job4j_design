5411f31b933cb2f3d14a016ef834866e5df32dd8
svoh86 <sms-86@mail.ru>
2022-03-25 16:51:24 +0300

    Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/forwardlinked/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/forwardlinked/ForwardLinked.java
@@ -43,6 +43,33 @@ public class ForwardLinked<T> implements Iterable<T> {
         return rsl;
     }
 
+    /**
+     * Метод переворачивает односвязный список.
+     * Валидация, что список не пуст и имеет больше одного значения.
+     * Указатель на предыдущий, текущий и следующий элементы.
+     * следующий = текущий.next;
+     * текущий.next = предыдущий;  разворот ссылки
+     * предыдущий = текущий;
+     * текущий = следующий;
+     *
+     * @return true or false
+     */
+    public boolean revert() {
+        if (head != null && head.next != null) {
+            Node<T> preNode = null;
+            Node<T> currentNode = head;
+            while (currentNode != null) {
+                Node<T> nextNode = currentNode.next;
+                currentNode.next = preNode;
+                preNode = currentNode;
+                currentNode = nextNode;
+            }
+            head = preNode;
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {

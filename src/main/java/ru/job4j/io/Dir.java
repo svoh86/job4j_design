2b7f3be9680244df33ee2e3729213966a5f29343
svoh86 <sms-86@mail.ru>
2022-04-14 10:55:19 +0300

    File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName() + " - " + subfile.length());
+        }
+    }
+}

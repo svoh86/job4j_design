1ba536e1ac6d9c7656ae840e767a6f89b8d7dd4f
svoh86 <sms-86@mail.ru>
2022-04-15 17:53:41 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -15,8 +15,11 @@ import java.util.function.Predicate;
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Enter path and extension");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     /**

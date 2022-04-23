6ef2fb411eac71c003eb130a37177208378c7310
svoh86 <sms-86@mail.ru>
2022-04-23 14:04:42 +0300

    Simple Loggin Facade 4 Java. [#268849]

---
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class UsageLog4j {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+
+}

16720d70b75dca7ea5e321d6f835baab76cfbf9f
svoh86 <sms-86@mail.ru>
2022-05-05 15:54:13 +0300

    Select [#6861]

---
+++ Psql/insert_fauna.sql
@@ -0,0 +1,4 @@
+insert into fauna (name, avg_age, discovery_date) values ('swordfish', 11000, date '01.11.1920');
+insert into fauna (name, avg_age, discovery_date) values ('tiger', 20000, date '29.07.1070');
+insert into fauna (name, avg_age, discovery_date) values ('lion', 23000, date '19.05.2020');
+insert into fauna (name, avg_age) values ('volf', 9000);

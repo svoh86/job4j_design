49b7d09ab2a7ed04e199cf8e4b1fc9e1214409ef
svoh86 <sms-86@mail.ru>
2022-06-18 13:39:00 +0300

    Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CinemaTest {
+
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 6, 18, 10, 45);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    /**
+     * Тест, когда место занято или не существует.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyFail() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 6, 18, 10, 45);
+        Ticket ticket = cinema.buy(account, 10, 10, date);
+    }
+
+    /**
+     * Тест, когда дата некорректная.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDateWrong() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2030, 6, 18, 10, 45);
+    }
+}

39bc7ea5b4bddc6c7e682bf6e581782709c4406e
svoh86 <sms-86@mail.ru>
2022-06-18 13:43:06 +0300

    Что такое TDD? [#4918 #313701]

---
+++ src/main/java/ru/job4j/tdd/Session.java
@@ -0,0 +1,8 @@
+package ru.job4j.tdd;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public interface Session {
+}
---
+++ src/main/java/ru/job4j/tdd/Ticket3D.java
@@ -0,0 +1,8 @@
+package ru.job4j.tdd;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class Ticket3D implements Ticket {
+}

7a9cf3bf85699dae02a209e908caf1fecc1f0ca0
svoh86 <sms-86@mail.ru>
2022-06-21 14:53:27 +0300

    Отчеты. [#850 #314551]

--- src/main/java/ru/job4j/ood/srp/reports/generator/GeneratorReport.java
+++ src/main/java/ru/job4j/ood/srp/reports/generator/GeneratorReport.java
@@ -7,7 +7,5 @@ import java.text.SimpleDateFormat;
 import java.util.function.Predicate;
 
 public interface GeneratorReport {
-//    SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd:MM:yyyy HH:mm");
-
     String generate(Predicate<Employee> filter, Store store);
 }
